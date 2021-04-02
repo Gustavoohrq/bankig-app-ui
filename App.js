413d6d9b53f578929a71d114118dc5a6f7601f09
Gustavo Henrique <gustavofmariano@gmail.com>
2021-04-02 16:49:16 -0300

    UI Banking APP

--- App.js
+++ App.js
@@ -1,21 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
+import TouchScreen from './src/screens/TouchScreen'
 
 export default function App() {
+  const AppStack = createStackNavigator()
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <AppStack.Navigator headerMode="none">
+        <AppStack.Screen name="Touch" component={TouchScreen}/>
+      </AppStack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
